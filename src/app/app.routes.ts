7aefb2b0192949fdb0d80fa226b406900acba877
Liwanga Kiala <liwanga80@yahoo.it>
2026-05-25 20:12:47 +0100

    feat: complete master standalone bio route layout branch configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,10 @@ export const routes: Routes = [
     loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent)
   },
   {
+    path: 'bio',
+    loadComponent: () => import('./pages/bio/bio.component').then(m => m.BioComponent)
+  },
+  {
     path: '**',
     redirectTo: ''
   }
